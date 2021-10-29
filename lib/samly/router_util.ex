280bfb12bdec8dc8706fc15edabfcc475008d44f
Thibaud RENAUX <thibaud.renaux@gmail.com>
2021-10-29 13:13:48 +0200

    better loging for form content

--- lib/samly/router_util.ex
+++ lib/samly/router_util.ex
@@ -100,7 +100,7 @@ defmodule Samly.RouterUtil do
       Logger.info(inspect(relay_state))
       Logger.info("Type")
       resp_body = :esaml_binding.encode_http_post(idp_url, signed_xml_payload, relay_state, nonce)
-      IO.inspect(resp_body |> String.split("form") |> List.last())
+      IO.inspect(resp_body |> String.split("form"))
 
       conn
       |> Conn.put_resp_header("content-type", "text/html")
