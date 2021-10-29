0b8b76cb0b7d6997e6b41fc2da6f13db4e04342a
Thibaud RENAUX <thibaud.renaux@gmail.com>
2021-10-29 10:42:27 +0200

    usel ogger instead of io

--- lib/samly/router_util.ex
+++ lib/samly/router_util.ex
@@ -93,7 +93,7 @@ defmodule Samly.RouterUtil do
     else
       nonce = conn.private[:samly_nonce]
       resp_body = :esaml_binding.encode_http_post(idp_url, signed_xml_payload, relay_state, nonce)
-      IO.inspect(resp_body)
+      Logger.info(inspect(resp_body))
 
       conn
       |> Conn.put_resp_header("content-type", "text/html")
