28405b5498f52462d534e13a4c01272d3b6e4528
Thibaud RENAUX <thibaud.renaux@gmail.com>
2022-01-25 17:56:53 +0100

    add more debug

--- lib/samly/sp_handler.ex
+++ lib/samly/sp_handler.ex
@@ -94,6 +94,11 @@ defmodule Samly.SPHandler do
     idp_id_in_session = get_session(conn, "idp_id")
     url_in_session = get_session(conn, "target_url")
 
+    IO.inspect(idp_id, label: "validate_authresp_idp_id")
+    IO.inspect(rs_in_session, label: "validate_authresp_rs_in_session")
+    IO.inspect(idp_id_in_session, label: "validate_authresp_idp_id_in_session")
+    IO.inspect(url_in_session, label: "validate_authresp_url_in_session")
+
     cond do
       rs_in_session == nil || rs_in_session != relay_state ->
         {:error, :invalid_relay_state}
