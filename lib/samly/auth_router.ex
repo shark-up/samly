4c855de7cdc4a9cbe0e1a503c0cc0235fd79ee91
Thibaud RENAUX <thibaud.renaux@gmail.com>
2022-01-27 11:08:11 +0100

    define separate csrf pipelines

--- lib/samly/auth_router.ex
+++ lib/samly/auth_router.ex
@@ -5,15 +5,26 @@ defmodule Samly.AuthRouter do
   import Plug.Conn
   import Samly.RouterUtil, only: [check_idp_id: 2, check_target_url: 2]
 
-  plug :fetch_session
-  # plug Plug.CSRFProtection
-  plug :match
-  plug :check_idp_id
-  plug :check_target_url
-  plug :dispatch
+  pipeline :csrf_pipeline do
+    plug :fetch_session
+    plug Plug.CSRFProtection
+    plug :match
+    plug :check_idp_id
+    plug :check_target_url
+    plug :dispatch
+  end
+
+  pipeline :without_csrf_pipeline do
+    plug :fetch_session
+    # plug Plug.CSRFProtection
+    plug :match
+    plug :check_idp_id
+    plug :check_target_url
+    plug :dispatch
+  end
 
   scope "/signin" do
-    plug Plug.CSRFProtection
+    pipe_through(:csrf_pipeline)
 
     get "/*idp_id_seg" do
       conn |> Samly.AuthHandler.initiate_sso_req()
@@ -25,6 +36,8 @@ defmodule Samly.AuthRouter do
   end
 
   scope "/signout" do
+    pipe_through(:without_csrf_pipeline)
+
     get "/*idp_id_seg" do
       conn |> Samly.AuthHandler.initiate_sso_req()
     end
